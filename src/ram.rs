0df2332473b5a4659d1812c4fe17c5029f9dd3e1
Toni Le <toni.le@rwth-aachen.de>
2021-04-29 12:46:49 +0200

    Update filename and struct

---
+++ src/ram.rs
@@ -0,0 +1,20 @@
+const MEMORY_SIZE: usize = 0xFFF;
+pub struct Ram {
+    pub memory: [u8; MEMORY_SIZE],
+}
+
+impl Ram {
+    pub fn new() -> Ram {
+        let mut memory = [0; MEMORY_SIZE];
+
+        Ram { memory: memory }
+    }
+
+    pub fn write_byte(&mut self, address: u16, value: u8) {
+        self.memory[address as usize] = value;
+    }
+
+    pub fn read_byte(&self, address: u16) -> u8 {
+        self.memory[address as usize]
+    }
+}
