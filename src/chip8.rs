45c7f08f9ef6ff609b50aa20c16d3d2aeb9cd0af
Toni Le <toni.le@rwth-aachen.de>
2021-04-30 15:57:56 +0200

    Update chip8.rs

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,5 +1,19 @@
+use crate::{cpu::CPU, ram::Ram};
+
 pub struct Chip8 {
     cpu: CPU,
+    ram: Ram,
 }
 
-impl Chip8 {}
+impl Chip8 {
+    pub fn new() -> Chip8 {
+        let mut ram = Ram::new();
+        let mut cpu = CPU::new();
+
+        Chip8 { ram, cpu }
+    }
+
+    fn init_memory() {}
+
+    pub fn load_rom(program: Vec<u8>) {}
+}
