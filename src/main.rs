f5d99dae0b7a27bbb4b9d9e4dfc595c69685de95
Toni Le <toni.le@outlook.de>
2021-05-05 21:58:56 +0200

    Update main.rs
    Read and load rom into chip8 memory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod chip8;
+mod cpu;
+mod fonts;
+mod ram;
+
+use std::{fs::File, io::Read};
+
+use chip8::Chip8;
+
 fn main() {
-    println!("Hello, world!");
+    let input_rom = "rom/Space_Invaders.ch8";
+
+    let mut rom = File::open(input_rom).expect("Not ableto open rom");
+    let mut program = Vec::<u8>::new();
+    rom.read_to_end(&mut program).expect("Not able to read rom");
+
+    let mut chip8 = Chip8::new();
+    chip8.load_rom(program);
 }
