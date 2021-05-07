2133d9c4cd82c5a501729fa3bff8f9387572a829
Toni Le <toni.le@outlook.de>
2021-05-07 23:08:36 +0200

    Update instruction.rs

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,18 +1,76 @@
 #[derive(Debug)]
 pub enum Instruction {
-    JMP(u16),
+    Clear,
+    Return,
+    Jump(u16),
+    Call(u16),
+    SkipEqVxByte(u8, u8),
+    SkipNEqVxByte(u8, u8),
+    SkipEqVxVy(u8, u8),
+    LoadVxByte(u8, u8),
+    AddVxByte(u8, u8),
+    LoadVxVy(u8, u8),
+    OrVxVy(u8, u8),
+    AndVxVy(u8, u8),
+    Xor(u8, u8),
+    AddVxVy(u8, u8),
+    RShiftVx(u8),
+    SubnVxVy(u8, u8),
+    LShiftVx(u8),
+    SkipNEqVxVy(u8, u8),
+    LoadI(u8),
+    JumpV0(u8),
+    RndVxByte(u8, u8),
+    Draw(u8, u8, u8),
+    SkipVx(u8),
+    LoadVxTimer(u8),
+    LoadVxKey(u8),
+    LoadTimerVx(u8),
+    LoadSoundVx(u8),
+    AddI(u8),
+    LoadFVx(u8),
+    LoadBVx(u8),
+    StoreIVx(u8),
+    LoadIVx(u8),
 }
 
-pub fn translate_opcode(opcode: u16) -> Option<Instruction> {
-    match xoo(opcode) {
-        0x1 => Some(Instruction::JMP(nnn(opcode))),
-        _ => None,
+#[allow(dead_code)]
+impl Instruction {
+    pub fn translate_opcode(opcode: u16) -> Option<Instruction> {
+        match oxoo(opcode) {
+            0x0 => match kk(opcode) {
+                0xE0 => Some(Instruction::Clear),
+                0xEE => Some(Instruction::Return),
+                _ => None,
+            },
+            0x1 => Some(Instruction::Jump(nnn(opcode))),
+            0x2 => Some(Instruction::Call(nnn(opcode))),
+            0x3 => Some(Instruction::SkipEqVxByte(oxoo(opcode), kk(opcode))),
+            0x4 => Some(Instruction::SkipNEqVxByte(oxoo(opcode), kk(opcode))),
+            0x5 => Some(Instruction::SkipEqVxVy(oxoo(opcode), ooxo(opcode))),
+            0x6 => Some(Instruction::LoadVxByte(oxoo(opcode), kk(opcode))),
+            0x7 => Some(Instruction::AddVxByte(oxoo(opcode), kk(opcode))),
+            _ => None,
+        }
     }
 }
 
-fn xoo(opcode: u16) -> u8 {
+fn oxoo(opcode: u16) -> u8 {
     let shifted_value = opcode >> 12;
     (shifted_value) as u8
+}
+
+fn ooxo(opcode: u16) -> u8 {
+    let shifted_value = opcode >> 4;
+    (shifted_value & 0xF) as u8
+}
+
+fn ooox(opcode: u16) -> u8 {
+    (opcode & 0xF) as u8
+}
+
+fn kk(opcode: u16) -> u8 {
+    (opcode & 0xFF) as u8
 }
 
 fn nnn(opcode: u16) -> u16 {
@@ -29,13 +87,13 @@ mod tests {
         let test_code: u16 = 0x1FFF;
 
         // When
-        let test_instruction = translate_opcode(test_code);
+        let test_instruction = Instruction::translate_opcode(test_code);
 
         // Then
         match test_instruction {
             Some(instruction) => match instruction {
-                Instruction::JMP(0xFFF) => Ok(()),
-                Instruction::JMP(_) => Err(String::from("Failed to jump to correct address")),
+                Instruction::Jump(0xFFF) => Ok(()),
+                _ => Err(String::from("Failed to jump to correct address")),
             },
             None => Err(String::from("Failed to find JMP instruction")),
         }
