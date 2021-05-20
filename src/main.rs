01a06e8dfd925bbff7979c3b956ebf704b9cae8d
Toni Le <toni.le@outlook.de>
2021-05-20 20:48:12 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ mod ram;
 use std::{fs::File, io::Read};
 
 use chip8::Chip8;
+use piston::{
+    EventSettings, Events, PressEvent, ReleaseEvent, RenderEvent, UpdateEvent, WindowSettings,
+};
+use piston_window::PistonWindow as Window;
+
 fn main() {
     let input_rom = "rom/Space_Invaders.ch8";
 
@@ -19,4 +24,21 @@ fn main() {
     let mut chip8 = Chip8::new();
     chip8.load_rom(program);
     chip8.execute_cycle();
+
+    let mut window: Window = WindowSettings::new("Chip-8 emulator", (640, 480))
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut events = Events::new(EventSettings::new());
+
+    while let Some(event) = window.next() {
+        if let Some(key) = event.press_args() {}
+
+        if let Some(key) = event.release_args() {}
+
+        if let Some(update) = event.update_args() {}
+
+        if let Some(args) = event.render_args() {}
+    }
 }
