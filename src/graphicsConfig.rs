94dd1f692b976080a06d09c301d24d990015d800
Toni Le <toni.le@outlook.de>
2021-06-02 17:22:09 +0200

    Delete graphicsConfig.rs

--- src/graphicsConfig.rs
+++
@@ -1,9 +0,0 @@
-use glutin_window::GlutinWindow;
-use opengl_graphics::GlGraphics;
-use piston::Size;
-
-pub struct GraphicsConfig {
-    pub gl: GlGraphics,
-    pub settings: GlutinWindow,
-    pub size: Size,
-}
